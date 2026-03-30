ad81b0e870c8f0ab6d634930437241852a4d3dda
hafizh <alhafizh24si@mahasiswa.pcr.ac.id>
2026-03-30 10:03:42 +0700

    P3

---
+++ app/src/main/java/com/example/fzhmobile/pertemuan_3/ThirdResultActivity.kt
@@ -0,0 +1,32 @@
+package com.example.fzhmobile.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fzhmobile.R
+import com.example.fzhmobile.databinding.ActivityThirdBinding
+
+class ThirdResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_third_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        binding.btnKirim.setOnClickListener {
+//
+//
+//            val intent = Intent(this, ThirdResultActivity::class.java)
+//            startActivity(intent)
+      //  }
+    }
+}
