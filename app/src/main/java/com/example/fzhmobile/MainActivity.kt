fbc5e5342cfbec09434c03164d23f10d80d96240
A D V A N <alhafizh24si@mahasiswa.pcr.ac.id>
2026-04-14 09:33:45 +0700

    P5 Original Coding Modul

--- app/src/main/java/com/example/fzhmobile/MainActivity.kt
+++ app/src/main/java/com/example/fzhmobile/MainActivity.kt
@@ -9,34 +9,35 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.fzhmobile.databinding.ActivityMainBinding
 import com.example.fzhmobile.pertemuan_4.FourthActivity
+import com.example.fzhmobile.pertemuan_5.FifthActivity
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.buttonSend.setOnClickListener {
-            val btnButton = binding.buttonSend.text
-            Toast.makeText(this, "Pesan Berhasil Dikirim ke $btnButton", Toast.LENGTH_SHORT).show()
+        binding.btnKirim.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
+
+            /*tambahkan bagian berikut*/
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+
+            startActivity(intent)
         }
-
-        val intent = Intent(this, FourthActivity::class.java)
-
-        /*tambahkan bagian berikut*/
-        intent.putExtra("nama", "Politeknik Caltex Riau")
-        intent.putExtra("asal", "Rumbai")
-        intent.putExtra("umur", 25)
-
-        startActivity(intent)
-        finish()
+        binding.btnLima.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
         }
+    }
 }
